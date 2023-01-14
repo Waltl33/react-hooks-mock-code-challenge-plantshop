eeef1c05c332f41fd7efcc2d99972c0fee0d1167
Walter <walter.clark24@yahoo.com>
2023-01-14 18:52:04 -0500

    final

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+const [plants, setPlants] = useState([])
+const [searchItems, setSearchItems] = useState([])
+
+  useEffect(() => {
+    fetch (" http://localhost:6001/plants ")
+      .then(resp => resp.json())
+      .then(setPlants)
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search 
+        searchItems={searchItems}
+        onChangeSearchItems={setSearchItems}
+      />
+      <PlantList 
+        plants={plants}
+      />
     </main>
   );
 }
