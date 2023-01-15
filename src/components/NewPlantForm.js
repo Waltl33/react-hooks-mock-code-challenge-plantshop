54de13caca800391ee379b2d7de4432fbf6b8757
Walter <walter.clark24@yahoo.com>
2023-01-15 03:20:05 -0500

    final

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({addNewPlant}) {
+  const [newName, setName] = useState("")
+  const [newImage, setImage] = useState("")
+  const [newPrice, setPrice] = useState(0)
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    
+  let newPlant = {
+    name: newName,
+    image: newImage,
+    price: newPrice
+  }
+fetch("http://localhost:6001/plants",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPlant)
+
+})
+  .then (resp => resp.json())
+  .then(myNewPlants => addNewPlant(myNewPlants))
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value ={newName}onChange={(e) => setName(e.target.value)}placeholder="Plant name" />
+        <input type="text" name="image" value ={newImage}onChange={(e) => setImage(e.target.value)} placeholder="Image URL" />
+        <input type="number" name="price" value ={newPrice}onChange={(e) => setPrice(e.target.value)} step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants.map(plant => <PlantCard 
+      plant = {plant}
+      key = {plant.id}
+    />)}</ul>
   );
 }
+
 
 export default PlantList;
